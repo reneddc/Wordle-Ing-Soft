9c30b4c66a6ed8f4bc330ab2725cc53c1c6dfead
“Rene <rene.diaz@ucb.edu.bo>
2022-05-14 18:42:31 -0400

    (UI FAILED) <F> <HDU 2> <Test 1> <Click JUEGO RAPIDO y cambiar a vista CAMPO DE JUEGO>

--- src/wordle.test.js
+++ src/wordle.test.js
@@ -6,6 +6,9 @@ describe("F. DEFINIR MODALIDADES DE JUEGO", () => {
   it("1. En la vista “Modalidades de Juego” debería mostrar la descripción de la modalidad “Juego Rápido”: Adivinar una palabra cualquiera elegida al azar" , () => {
     //expect(sumar(3, 2)).toEqual(5);
   });
+
+  //HDU 2
+  it("1. Si se presiona el botón “Juego Rápido” se debería borrar la vista “Modalidades” y mostrar la vista “Campo de Juego”" , () => {});
 });
 
 
