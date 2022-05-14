d4f515d9749ef7c0f0d23e79182439f3ff5c06c0
“Rene <rene.diaz@ucb.edu.bo>
2022-05-14 17:40:43 -0400

    Commit inicial

--- src/presenter.js
+++ src/presenter.js
@@ -7,8 +7,9 @@ const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  
 
+
+  
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
