af385b8587532bfad5780b7db51bbee6e75afc77
“Rene <rene.diaz@ucb.edu.bo>
2022-05-14 19:04:45 -0400

    (TEST FAILED) <F> <HDU 3> <Test 1> <Crear una palabra seleccionable>

--- src/wordle.test.js
+++ src/wordle.test.js
@@ -3,14 +3,17 @@
 
 describe("F. DEFINIR MODALIDADES DE JUEGO", () => {
   //HDU 1
-  it("1. En la vista “Modalidades de Juego” debería mostrar la descripción de la modalidad “Juego Rápido”: Adivinar una palabra cualquiera elegida al azar" , () => {
-    //expect(sumar(3, 2)).toEqual(5);
-  });
+  it("1. En la vista “Modalidades de Juego” debería mostrar la descripción de la modalidad “Juego Rápido”: Adivinar una palabra cualquiera elegida al azar" , () => {});
 
   //HDU 2
   it("1. Si se presiona el botón “Juego Rápido” se debería borrar la vista “Modalidades” y mostrar la vista “Campo de Juego”" , () => {});
-});
 
+  //HDU 3
+  it("1. Crear y mostrar una palabra seleccionable para jugar" , () => {
+    expect(definirPalabrasSeleccionables("HOJA")).toEqual("HOJA");
+  });
+
+});
 
 
 
