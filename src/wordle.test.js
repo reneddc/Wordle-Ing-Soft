c5bc9207ebeab0d1679c300cdd9803f0f663fe6d
“Rene <rene.diaz@ucb.edu.bo>
2022-05-14 21:02:32 -0400

    (TEST FAILED) <G> <HDU 1> <Test 1> <Ingresar intento vacío>

--- src/wordle.test.js
+++ src/wordle.test.js
@@ -2,7 +2,7 @@ import Wordle from "./wordle"
 
 
 describe("F. DEFINIR MODALIDADES DE JUEGO", () => {
-  let wordle = new Wordle();
+  let wordleF = new Wordle();
   //HDU 1
   it("1. En la vista “Modalidades de Juego” debería mostrar la descripción de la modalidad “Juego Rápido”: Adivinar una palabra cualquiera elegida al azar" , () => {});
 
@@ -11,26 +11,40 @@ describe("F. DEFINIR MODALIDADES DE JUEGO", () => {
 
   //HDU 3
   it("1. Si se presiona el botón “Juego Rápido” y la palabra creada por el juego fue <HOLA>, debería mostrar un mensaje de texto con la palabra <HOLA>" , () => {
-    expect(wordle.obtenerPalabrasSeleccionables()[0]).toEqual("HOJA");
+    expect(wordleF.obtenerPalabrasSeleccionables()[0]).toEqual("HOJA");
   });
 
   //HDU 4
   it("1. Si se presiona el botón “Juego Rápido” y la lista de palabras creadas por el juego fue <HOLA, HIJA, DUKE>, debería mostrar un mensaje de texto con las palabras <HOLA, HIJA, DUKE>" , () => {
-    expect(wordle.obtenerPalabrasSeleccionables()).toEqual(["HOJA", "HOLA", "LOBO", "COLA", "PALO", "CINCO", "CIRCO", "PERRO", "DUKE"]);
+    expect(wordleF.obtenerPalabrasSeleccionables()).toEqual(["HOJA", "HOLA", "LOBO", "COLA", "PALO", "CINCO", "CIRCO", "PERRO", "DUKE"]);
   });
 
   //HDU 5
   it("1. Si se presiona el botón “Juego Rápido” y la lista de palabras creada por el juego fue <HOLA, HIJA, DUKE>, debería definirse como “Palabra Secreta” a <HOLA> y mostrarla en pantalla" , () => {
-    expect(wordle.obtenerPalabrasSeleccionables()[0]).toEqual("HOJA");
+    expect(wordleF.obtenerPalabrasSeleccionables()[0]).toEqual("HOJA");
   });
 
   //HDU 6
   it("1. Si se presiona el botón “Juego Rápido” y la lista de palabras creada por el juego fue <HOLA, HIJA, DUKE>, debería definirse al azar la “Palabra Secreta” y mostrarla" , () => {
-    wordle.definirPalabraSecreta();
-    let palabraSecreta = wordle.obtenerPalabraSecreta();
+    wordleF.definirPalabraSecreta();
+    let palabraSecreta = wordleF.obtenerPalabraSecreta();
     console.log(palabraSecreta);
     expect(palabraSecreta).toEqual(palabraSecreta);
   });
+});
+
+
+describe("G. INGRESAR INTENTOS", () => {
+  let wordleG = new Wordle();
+
+  //HDU 1
+  it("1. Si la palabra secreta es <HOLA> y se ingresa como intento la palabra <>, debería mostrar el mensaje “Palabra incompleta”" , () => {
+    wordleG.definirPalabraSecreta();
+    wordleG.ingresarIntento("");
+    let respuesta = wordleG.obtenerCadenaRespuesta();
+    expect(respuesta).toEqual("Palbra Incompleta");
+  });
+
 });
 
 
