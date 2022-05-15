ee288562e2a331e7853043d8ff1dfce49430964a
Fran Aliss <franalissar@gmail.com>
2022-05-15 13:30:22 -0400

    (UI) B.1 <Mostrar Vista Tutorial>

--- src/presenter.js
+++ src/presenter.js
@@ -18,23 +18,40 @@ let nroIntentos;
 const vistaModalidades = document.querySelector("#vista-modalidades");
 const vistaCampoJuego = document.querySelector("#vista-campo-juego");
 const intentosRealizados = document.querySelector("#intentos-realizados");
+const vistaTutorial = document.querySelector("#vista-tutorial");
+const vistaPantallaPrincipal = document.querySelector("#vista-Principal");
 
 
 //FORMULARIOS
 const formJuegoRapido = document.querySelector("#juego-rapido-form");
 const formIntento = document.querySelector("#ingresar-intento-form");
 const formHistorialIntentos = document.querySelector("#historial-intentos-form");
+const formTutorial = document.querySelector("#tutorial-form");
+const formModalidades = document.querySelector("#modalidades-form");
+const formJuegoRapidoPrincipal = document.querySelector("#juego-rapido-form-principal");
 
 
 //GenerarVistas
+function mostrarVistaModalidades(){
+  vistaModalidades.style.display = "block";
+}
+
 function mostrarVistaCampoJuego(){
   formIntento.innerHTML = generarHtmlIngresarIntento(tamPalabraSecreta);
   refrescarHistorialIntentos();
   vistaCampoJuego.style.display = "block";
 }
 
+function mostrarVistaTutorial(){
+  vistaTutorial.style.display = "block";
+}
+
 function ocultarVistaModalidades(){
   vistaModalidades.style.display = "none";
+}
+
+function ocultarVistaPrincipal(){
+  vistaPantallaPrincipal.style.display = "none";
 }
 
 function mostrarIntentosRealizados(){
@@ -63,10 +80,24 @@ formJuegoRapido.addEventListener("submit", (event) => {
   alert("Palabra Secreta: " + palabraSecreta);
 
   listaIntentos = wordle.obtenerHistorialIntentos();
+  ocultarVistaPrincipal();
   ocultarVistaModalidades();
   mostrarVistaCampoJuego();
 });
 
+formJuegoRapidoPrincipal.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  wordle.definirPalabraSecreta();
+  palabraSecreta = wordle.obtenerPalabraSecreta();
+  tamPalabraSecreta = palabraSecreta.length;
+  alert("Palabra Secreta: " + palabraSecreta);
+
+  listaIntentos = wordle.obtenerHistorialIntentos();
+  ocultarVistaPrincipal();
+  ocultarVistaModalidades();
+  mostrarVistaCampoJuego();
+});
 
 formIntento.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -81,4 +112,16 @@ formIntento.addEventListener("submit", (event) => {
   nroIntentos = wordle.obtenerNroIntentos();
   mostrarIntentosRealizados();
 
+});
+
+formTutorial.addEventListener("submit", (event) => {
+  event.preventDefault();
+  ocultarVistaPrincipal();
+  mostrarVistaTutorial();
+});
+
+formModalidades.addEventListener("submit", (event) => {
+  event.preventDefault();
+  ocultarVistaPrincipal();
+  mostrarVistaModalidades();
 });
