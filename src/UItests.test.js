cac38bcc378e80ac7938edb8c83f8114c35b25da
Fran Aliss <franalissar@gmail.com>
2022-05-30 23:54:30 -0400

    <PROBANDO UI> <CONTINUARA....>

--- src/UItests.test.js
+++ src/UItests.test.js
@@ -35,8 +35,6 @@ describe("VISTAS", () => {
     expect(stateNone).toEqual("none");
     expect(stateBlock).toEqual("block");
   });
-
- 
 });  
 
 
@@ -49,16 +47,65 @@ describe("OBTENER VALORES", () => {
   });
 
   it("No se muestra ningun color si no se hizo un intento", () => {
-    
-    const botonJuegoRapido = document.querySelector("#boton-juego-rapido");
+    let botonJuegoRapido = document.querySelector("#boton-juego-rapido");
     botonJuegoRapido.click();
 
-    const casillasIntento = document.querySelectorAll(".ingresar-intento");
-    for(let i=0; i<casillasIntento.length; i++){
-      expect(casillasIntento[i].value).toEqual("");
-    }
+    let botonIngresarIntento = document.querySelector(".ingresar-palabra-boton");
+    expect(botonIngresarIntento.value).toEqual("Hola");
+    //let casillaIntento = document.querySelectorAll("#input-0");
+    let casillasIntento = document.querySelectorAll(".ingresar-intento");
+    //casillaIntento.value = "H";
+    
+    //for(let i=0; i<casillasIntento.length; i++){
+      //expect(casillasIntento.length).toEqual(4);
+      //const historialIntentos =document.querySelectorAll(".fila-1")
+      //expect(historialIntentos.length).toEqual(4);
+      //expect(casillasIntento[i].value).toEqual("");
+    //}
   });
-});  
 
+  it("Se muestra el primer intento ingresado", () => {
+    /*let botonJuegoRapido = document.querySelector("#boton-juego-rapido");
+    botonJuegoRapido.click();
+
+    let botonIngresarIntento = document.querySelector(".ingresar-palabra-boton");
+    let palabraAdivinar = document.querySelectorAll(".ingresar-intento");
+    
+    let tam = palabraAdivinar.length;
+    /*palabraAdivinar[0].value = "P";
+    palabraAdivinar[1].value = "A";
+    palabraAdivinar[2].value = "L";
+    palabraAdivinar[3].value = "O";
+    if(palabraAdivinar.length > 4){
+      palabraAdivinar[4].value = "S";
+    }
+
+    botonIngresarIntento.click();
+
+    const casillasIntento = document.querySelectorAll(".ingresar-intento");
+    const historialIntentos =document.querySelectorAll(".fila-1")
+
+    for(let i=0; i<casillasIntento.length; i++){
+      expect(historialIntentos[i].value).toEqual(palabraAdivinar[i].value);
+    }
+    expect(tam).toEqual(4);*/
+    /*const botonJuegoRapido = document.querySelector("#boton-juego-rapido");
+    botonJuegoRapido.click();
+
+    const palabraAdivinar = document.querySelectorAll(".ingresar-intento");
+    const botonIngresarIntento = document.querySelector(".ingresar-palabra-boton");
+    palabraAdivinar[0].value = "C";
+    palabraAdivinar[1].value = "O";
+    palabraAdivinar[2].value = "L";
+    palabraAdivinar[3].value = "A";
+
+    botonIngresarIntento.click();
+
+    const historialIntentos =document.querySelectorAll(".fila-1")
+    const resultado = historialIntentos[0].style.background;
+    expect(resultado).toEqual("rgb(194, 192, 192)");*/
+  });
+
+});  
 
 //<TEST UI> <Borrar la pantalla principal y que muetre el campo de juego>
